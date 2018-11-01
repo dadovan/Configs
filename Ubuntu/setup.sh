399179d413fd44e1c370dc0c9f4aa1d9324eb81d
Donovan Smith <dadovan@live.com>
2018-10-31 23:14:58 -0700

    Added VSCode

--- Ubuntu/setup.sh
+++ Ubuntu/setup.sh
@@ -15,6 +15,15 @@ apt-get install net-tools
 apt-get install terminator
 apt-get install tree
 
+# Install Visual Studio Code
+# https://code.visualstudio.com/docs/setup/linux
+curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
+install -o root -g root -m 644 microsoft.gpg /etc/apt/trusted.gpg.d/
+sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
+apt-get install apt-transport-https
+apt-get update
+apt-get install code
+
 # Maybe adds
 #apt-get install glances
 #apt-get install krusader
