a9300879d6829aefdd4b9dc69ba29cfce52259da
Donovan Smith <dadovan@live.com>
2022-07-06 08:30:30 -0700

    Added starship config

--- WSL/setup.sh
+++ WSL/setup.sh
@@ -38,15 +38,19 @@ git clone https://github.com/zsh-users/zsh-completions.git ~/.config/zsh/zsh-com
 cp -r ~/git/Configs/.config/zsh ~/.config/
 chsh -s /bin/zsh
 
+# Starship prompt
+curl -sS https://starship.rs/install.sh | sh
+
 ###################################################################################################
 # Apps
 
 sudo apt install bat                        # cat replacement @ https://github.com/sharkdp/bat
+sudo apt install btop                       # Top replacement @ https://github.com/aristocratos/btop
 sudo apt install exa                        # ls replacement @ https://the.exa.website/
 sudo apt install fzf                        # Command-line fuzzy finder @ https://github.com/junegunn/fzf
-# sudo apt install mc                         # Midnight Commander
+# sudo apt install mc                       # Midnight Commander
 sudo apt install micro                      # Micro editor @ https://micro-editor.github.io/
 sudo apt install ncdu                       # NCurses Disk Usage @ https://dev.yorhel.nl/ncdu
 sudo apt install neofetch
 sudo apt install ranger                     # Ranger file manager @ https://ranger.github.io/
-# sudo apt install stacer                     # Sytem monitoring and config @ https://oguzhaninan.github.io/Stacer-Web/
+# sudo apt install stacer                   # Sytem monitoring and config @ https://oguzhaninan.github.io/Stacer-Web/
