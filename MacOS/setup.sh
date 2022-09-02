88082f96bd0582e6d61f0847abb47ba3267dc2a7
donovans <donovans@zillowgroup.com>
2022-09-02 11:52:50 -0400

    Updated macOS setup

--- MacOS/setup.sh
+++ MacOS/setup.sh
@@ -3,17 +3,29 @@ brew install btop
 brew install exa
 brew install fzf
 brew install git-delta
+brew install gitui
+brew install glow			# https://github.com/charmbracelet/glow
+brew install hammerspooon	# http://www.hammerspoon.org/
 brew install kitty
 brew install lazydocker
 brew install lnav			# Logfile Navigator @ https://lnav.org
 brew install mc
 brew install micro
+brew install ncdu
+brew install p7zip
 brew install ripgrep        # Better grep @ https://github.com/BurntSushi/ripgrep
 brew install starship
 brew install tldr
 brew install tmux
+brew install wtfutil		# Terminal Dashboad @ https://wtfutil.com/
 brew install xplr			# ???
 brew install zoxide			# Cd replacement
+
+# https://www.brow.sh/
+brew tap browsh-org/homebrew-browsh
+brew install browsh
+brew install firefox
+brew install --cask alt-tab		# https://alt-tab-macos.netlify.app/
 
 echo "ZDOTDIR=$HOME/.config/zsh\
 . $ZDOTDIR/.zshenv" > ~/.zshenv
